4bfc4d081f6c10aded216ecde220aaa7170d475a
SaloniMishra <83867441+SaloniMishra@users.noreply.github.com>
2021-06-13 19:14:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import BookMySeats from './component/BookMySeats';
+
+class App extends Component {
+    render () {
+        return (
+            <BookMySeats/>
+        );
+    }
+};
+export default App;
